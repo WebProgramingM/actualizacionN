4200602fe20450998ab80d22df8dc035cbbf06dd
Oscar Javier <oscarmosh@gmail.com>
2016-11-23 15:42:18 -0500

    probando git

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
 			'img/gallery/06.jpg',
 			'img/gallery/07.jpg',
 			'img/gallery/08.jpg',
-			// 'img/gallery/09.jpg',
+			'img/gallery/09.jpg',
 			'img/gallery/10.jpg',
-			'img/gallery/11.jpg',
+			// 'img/gallery/11.jpg',
 			'img/gallery/12.jpg',
 			'img/gallery/13.jpg',
 			'img/gallery/14.jpg'
