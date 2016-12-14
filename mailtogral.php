2324cad352a75f7863998369243058bfaa153556
Luis Angel Zetina Irola <luanzeir@gmail.com>
2016-12-14 13:35:56 -0500

    Isset

--- mailtogral.php
+++ mailtogral.php
@@ -1,25 +1,35 @@
 <?php 
-	
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
 	$response = array();
-	// $date = $_POST['date'];
-	// $people = $_POST['people'];
-	// $time = $_POST['time'];
-
-
-	$to = "contactoweb@nomadscancun.com";
-	$subject = "CONTACT MESSAGE FROM WEB - NOMADS CANCUN";
-
-	$msg = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
-	if (mail($to, $subject, $msg)) {
-		$response['success'] = true;
-        $response['message'] = "Send Correct";
-	}else{
+	if(!isset($_POST['name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['message'])) {
+ 
         $response['success'] = false;
-        $response['message'] = "There it was a problem sending your message, please try again.";
+        $response['message'] = "There it was a problem sending your message, please try again.";       
+ 
+    } else{
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		// $date = $_POST['date'];
+		// $people = $_POST['people'];
+		// $time = $_POST['time'];
 
-	}
-    echo json_encode($response);
+
+		$to = "contactoweb@nomadscancun.com";
+		$subject = "CONTACT MESSAGE FROM WEB - NOMADS CANCUN";
+
+		$msg = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
+		if (mail($to, $subject, $msg)) {
+			$response['success'] = true;
+	        $response['message'] = "Send Correct";
+		}else{
+	        $response['success'] = false;
+	        $response['message'] = "There it was a problem sending your message, please try again.";
+
+		}
+	    echo json_encode($response);
+    }
 ?>
