93b70864f36181b476c7e0ba6de02ae18cd0fef5
Oscar Javier <oscarmosh@gmail.com>
2016-12-15 22:05:35 -0500

    landing nye

--- new-years-eve/gulpfile.js
+++ new-years-eve/gulpfile.js
@@ -1,63 +1,40 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
-var browserSync = require('browser-sync');
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
 
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-       baseDir: "./"
-    }
-  });
-});
-
-gulp.task('bs-reload', function () {
-  browserSync.reload();
+gulp.task('styles', function(){
+  return gulp.src('./assets/styles/main.styl')
+  .pipe(plumber())
+  .pipe(stylus({
+    compress: true
+  }))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(plumber.stop())
+  .pipe(gulp.dest('./css/'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('images', function(){
-  gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images/'));
+  gulp.src('./img/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./img/'));
 });
 
-gulp.task('styles', function(){
-  gulp.src(['src/styles/**/*.styl'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(stylus())
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-    .pipe(gulp.dest('dist/styles/'))
-    .pipe(browserSync.reload({stream:true}))
+// Static Server + watching stylus/php files
+gulp.task('serve', ['styles'], function() {
+    browserSync.init({
+    proxy: "http://local.nomadscancun.com:8888/new-years-eve",
+    notify: false
+    });
+    gulp.watch("./assets/styles/*.styl", ['styles']);
+    gulp.watch("./js/*.js").on('change', browserSync.reload);
+    gulp.watch(["./*.html", "./locations/array.php"]).on('change', browserSync.reload);
 });
 
-gulp.task('scripts', function(){
-  return gulp.src('src/scripts/**/*.js')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/scripts/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts/'))
-    .pipe(browserSync.reload({stream:true}))
-});
 
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("src/styles/**/*.styl", ['styles']);
-  gulp.watch("src/scripts/**/*.js", ['scripts']);
-  gulp.watch("*.html", ['bs-reload']);
-});
+gulp.task('default', ['serve']);
+
+
